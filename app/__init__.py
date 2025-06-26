20b5c14b21ac9dbc5e59c4e2c69f9722e349aab3
Matheus Mariano Rocha <matheusmarianomtd@gmail.com>
2025-06-25 21:54:41 -0300

    instalação CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from .config import Config
 from .db import db
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(Config)
     app.config['JSON_SORT_KEYS'] = False
 
