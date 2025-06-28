29b1c6654167db5e834a9257cc28c67dcc1d30f2
Matheus Mariano Rocha <matheusmarianomtd@gmail.com>
2025-06-28 17:18:58 -0300

    feature: função de exibir os inputs para adicionar novas tarefas ao db incluidas

--- frontend/script.js
+++ frontend/script.js
@@ -8,10 +8,46 @@ async function carregarTarefas() {
 
     data.tarefas.forEach(tarefa => {
       const item = document.createElement('li');
-      item.innerHTML = `<strong>${tarefa.titulo}</strong>: ${tarefa.descricao}`;
+      item.innerHTML = `<strong>ID: ${tarefa.cod_id}</strong> <strong>${tarefa.titulo}</strong>: ${tarefa.descricao}`;
       lista.appendChild(item);
     });
   } catch (erro) {
     console.error('Erro ao carregar tarefas:', erro);
   }
 }
+
+async function adicionarTarefa() {
+  const titulo = document.getElementById('titulo').value;
+  const descricao = document.getElementById('descricao').value;
+
+  if (!titulo || !descricao) {
+    alert('Por favor, preencha todos os campos.');
+    return;
+  }
+
+  try {
+    const resposta = await fetch('http://127.0.1:5000/tarefas', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ titulo, descricao })
+    });
+    if (!resposta.ok) {
+      throw new Error('Erro ao adicionar tarefa');
+    }
+    const data = await resposta.json();
+    console.log('Tarefa adicionada:', data);
+    document.getElementById('titulo').value = '';
+    document.getElementById('descricao').value = '';
+    carregarTarefas();
+  } catch (erro) {
+    console.error('Erro ao adicionar tarefa:', erro);
+    alert('Erro ao adicionar tarefa. Por favor, tente novamente.');
+  }
+}
+
+  document.getElementById('adicionar_tarefa').addEventListener('click', function() {
+  document.getElementById('container_input_tarefas').style.display = 'block';
+});
+
